baf37fddd62e00642a25ec8f56084e77dd5d2fc6
Jonathan Canola <jonathancanola7@gmail.com>
2023-11-09 11:48:08 -0800

    setup

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import ReactDOM  from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      name: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    };
+  } 
   render() {
     return (
       <div>
