89b5fa3ccecf2affbd4a6836e36297469ff52c0c
Jonathan Canola <jonathancanola7@gmail.com>
2023-11-10 10:52:26 -0800

    finished components todo, todolist, need to finish form'

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,24 +1,49 @@
 import React from 'react'
-import ReactDOM  from 'react'
+import TodoList from './TodoList';
+import Form from './Form';
+
+let id = 0
+let getId = () => ++id
+
+const initialTodos = [
+  {
+    name: 'Organize Garage',
+    id: getId(),
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: getId(),
+    completed: false
+  },
+];
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'Organize Garage',
-      id: 1528817077286,
-      completed: false
-    },
-    {
-      name: 'Bake Cookies',
-      id: 1528817084358,
-      completed: false
-    };
+  state = {
+    todos: initialTodos
   } 
+
+  addTodo = ({ name }) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({ id: id, completed: false, name })
+    })
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if (id == td.id) return { ...td, completed: !td.completed}
+        return td
+      })
+    })
+  }
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
+        <Form addTodo={this.addTodo}/>
       </div>
     )
   }
